270ee31aaada73dafaa62d2e425c4739d58c8683
blackchestnut <alexander.kalinichev@gmail.com>
2014-05-07 23:45:06 +0400

    dashborad page draft

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,12 @@
 class LandingController < ApplicationController
+  before_action :check_authorize
+
   def index
     @user = User.new
+  end
+
+private
+  def check_authorize
+    redirect_to dashboard_url if current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'landing#index'
 
+  resource :dashboard, only: :show
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
