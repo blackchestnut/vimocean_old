0a211bc729cdab2f479a69dcb100e436d579b33d
blackchestnut <alexander.kalinichev@gmail.com>
2014-07-19 23:47:10 +0400

    calendar header

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,8 @@
 class CalendarsController < ApplicationController
   def show
+    @date = Time.zone.today
+    @first_date = @date.beginning_of_week
+    @last_date = @first_date + 6
+    @week = (@first_date..@last_date)
   end
 end
