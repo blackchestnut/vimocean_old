4cc510e410887ed1e606e0fd76ef4b76f085eadd
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-03 21:49:24 +0300

    #13 Closest task list on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,7 @@
 class DashboardsController < ApplicationController
   def show
+    @overdue_schedules = current_user.schedules.includes(:task).where(state: :pending).where('finished_at <= ?', Time.zone.now)
+    @closest_schedules = current_user.schedules.includes(:task).where(state: :pending).where('finished_at > ?', Time.zone.now).where('started_at < ?', Time.zone.tomorrow)
+    @tomorrow_schedules = current_user.schedules.includes(:task).where(state: :pending).where('started_at >= ?', Time.zone.tomorrow).where('started_at <= ?', Time.zone.tomorrow.end_of_day)
   end
 end
