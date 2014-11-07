f898237e23ec31be7586703da89766f76cd3ab25
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-07 08:09:08 +0300

    #16 Fix run spec and add guard configs

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,11 +1,11 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :schedule do
-    task ""
     started_at "2014-06-02 00:09:03"
     finished_at "2014-06-02 00:09:03"
     priority 1
-    state "MyString"
+    state :pending
+    user nil
+    role nil
+    task nil
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    sequence :email do |n|
+      "test#{n}@email.com"
+    end
+    password '123456'
   end
 end
