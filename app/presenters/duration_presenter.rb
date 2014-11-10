429f22ace36ab29e0e5acdc0aa60c36bb8ff71cc
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-10 08:24:13 +0300

    #22 More info about tasks on dashboard

--- app/presenters/duration_presenter.rb
+++ app/presenters/duration_presenter.rb
@@ -1,8 +1,9 @@
 class DurationPresenter < Keynote::Presenter
   presents :object
+  delegate :finished_at, :started_at, to: :object
 
   def ui
-    min = ((object.finished_at - object.started_at) / 60).round
+    min = ((finished_at - started_at) / 60).round
     if min < 60
       "#{min}m"
     else
@@ -12,11 +13,20 @@ class DurationPresenter < Keynote::Presenter
   end
 
   def height_scale
-    scale = ((object.finished_at - object.started_at) / 3600).round(1)
+    scale = ((finished_at - started_at) / 3600).round(1)
     [1, scale].max
   end
 
   def task_min_height
     (30 * height_scale).to_i
+  end
+
+  def interval_ui
+    "#{adaptive_ui(started_at)}..#{finished_at.strftime('%H:%M')}"
+  end
+
+  def adaptive_ui(time)
+    format = time.today? ? '%H:%M' : '%b %d %H:%M'
+    time.strftime(format)
   end
 end
