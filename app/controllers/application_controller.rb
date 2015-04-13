b0b3dacaf85129a78acf907d07c4059f7ba4bab3
blackchestnut <alexander.kalinichev@gmail.com>
2015-04-13 10:55:21 +0300

    Redirect after session expired #29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   around_filter :user_time_zone, if: :current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :check_sign_in, unless: :devise_controller?
 
-protected
+  protected
 
-def configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
-private
+  private
+
   def user_time_zone(&block)
     Time.use_zone(current_user.time_zone, &block)
+  end
+
+  def check_sign_in
+    redirect_to root_url if !user_signed_in? && params[:controller] != 'landing'
   end
 end
